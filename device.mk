d196aafc0b9b5fdb2b02d84880f4261e3934ceb0
Christian Oder <myself5@carbonrom.org>
2018-07-15 21:31:07 +0200

    lmi: Add basic permission file for HotwordEnrollment
    Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

--- device.mk
+++ device.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # IFAA manager
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
