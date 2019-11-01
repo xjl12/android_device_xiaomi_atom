6dcc68ec418effb0206234169a4d7522466f57f2
TheScarastic <warabhishek@gmail.com>
2019-11-01 22:48:20 +0100

    lmi: parts: Add FOD service
    Change-Id: Ibc4ec25057d260e2dba9c221f0c79e6485ddf56d

--- parts/Android.mk
+++ parts/Android.mk
@@ -24,6 +24,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
+    vendor.xiaomi.hardware.displayfeature-V1.0-java \
     vendor.xiaomi.hardware.motor-V1.0-java
 
 LOCAL_RESOURCE_DIR := \
--- parts/src/org/mokee/settings/BootCompletedReceiver.java
+++ parts/src/org/mokee/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.mokee.settings.doze.DozeUtils;
+import org.mokee.settings.fod.FodUtils;
 import org.mokee.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -34,6 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
+        FodUtils.startService(context);
         PopupCameraUtils.startService(context);
     }
 }
