19ea7f17b12ee2486d3bf177fa2743a34791c73d
TheScarastic <warabhishek@gmail.com>
2020-05-11 10:10:47 +0000

    lmi: Add empty audio_policy_engine_configuration.xml
     * breaks compressed_offload
    
    Change-Id: Ib566c1e301b8ae95b18b3f82c655ad319b269576

--- device.mk
+++ device.mk
@@ -54,6 +54,9 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     libaacwrapper
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_engine_configuration.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti
