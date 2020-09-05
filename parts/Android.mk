f6957fdd24aab59fb493faefe26f7b40f7f5eb5d
TH779 <tongsj2333@gmail.com>
2020-09-05 22:24:11 +0800

    lmi: Import XiaomiParts from sdm845-common
     * From https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/65f0dd1a73e2088ceeca6922adef90fde21420e9.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>

---
+++ parts/Android.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := XiaomiParts
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    androidx.core_core \
+    androidx.preference_preference
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(TOP)/packages/resources/devicesettings/res
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include frameworks/base/packages/SettingsLib/common.mk
+
+include $(BUILD_PACKAGE)
