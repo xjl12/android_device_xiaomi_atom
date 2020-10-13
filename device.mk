f890a82d3c46efe0c1e449635d9c096377bb75c9
TH779 <tongsj2333@gmail.com>
2020-10-14 07:56:51 +0800

    lmi: Override camera provider rc file using vendor_overlay
     * Qualcomm modified camera provider rc file will cause camera error.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- device.mk
+++ device.mk
@@ -65,6 +65,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+PRODUCT_COPY_FILES += \
+    hardware/interfaces/camera/provider/2.4/default/android.hardware.camera.provider@2.4-service_64.rc:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/android.hardware.camera.provider@2.4-service_64.rc
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
