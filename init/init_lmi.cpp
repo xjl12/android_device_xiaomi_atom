acc87a2842d4867160c12a4a37bc1870d060e0c8
TH779 <tongsj2333@gmail.com>
2020-10-16 23:38:00 +0800

    lmi: Add custom libinit to set correct variants
    Signed-off-by: TH779 <tongsj2333@gmail.com>

---
+++ init/init_lmi.cpp
@@ -0,0 +1,55 @@
+/*
+   Copyright (c) 2020, The LineageOS Project
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <fstream>
+
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+
+using android::base::GetProperty;
+using android::init::property_set;
+
+void vendor_load_properties() {
+    std::string region = GetProperty("ro.boot.hwc", "");
+    std::string product = GetProperty("ro.boot.product.hardware.sku", "");
+
+    if (region.find("CN") != std::string::npos) {
+        if (product.find("pro") != std::string::npos) {
+            property_set("ro.product.model", "Redmi K30 Pro Zoom Edition");
+        } else {
+            property_set("ro.product.model", "Redmi K30 Pro");
+        }
+    } else if (region.find("GLOBAL") != std::string::npos) {
+        property_set("ro.product.model", "POCO F2 Pro");
+    }
+}
