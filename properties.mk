63d54243b2675563aa7b01e68b99596c5f6ba856
TH779 <tongsj2333@gmail.com>
2020-10-31 09:47:00 +0800

    lmi: Cleanup duplicate properties
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- properties.mk
+++ properties.mk
@@ -2,37 +2,19 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     audio.offload.min.duration.secs=30
 
-# Bluetooth
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.bluetooth.library_name=libbluetooth_qti.so
-
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     vendor.camera.aux.packagelist=org.lineageos.snap,org.codeaurora.snapcam
 
-# CNE
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.vendor.cne.feature=1
-
 # Display
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.latch_unsignaled=1
-
-# DPM
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.vendor.dpm.feature=1
+    debug.sf.enable_hwc_vds=1
 
 # IMS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
-
-# Netmgr
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.vendor.use_data_netmgrd=true \
-    persist.vendor.data.mode=concurrent
 
 # Telephony
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
