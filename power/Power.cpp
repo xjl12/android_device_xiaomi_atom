20a37e355daa2bacb8a5a004ef8c7331e38c1949
Bruno Martins <bgcngm@gmail.com>
2021-01-01 20:47:14 +0800

    lmi: power: Scan input devices for eligible DT2W node
    It is no longer a surprise for anyone to see Xiaomi shipping the same
    device with different displays and different displays usually mean
    different touchscreen drivers.
    
    The current double tap handling in the power HAL only takes into
    account one specific node, meant to be set in the device tree.
    However, if the touchscreen was registered as a different input device
    it would cause the feature to be broken. Address the issue by iterating
    through the input devices in order to find one supported touchscreen
    input to be used.
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- power/Power.cpp
+++ power/Power.cpp
@@ -29,8 +29,11 @@
 
 #define LOG_TAG "QTI PowerHAL"
 
+#include <linux/input.h>
+
 #include "Power.h"
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
 
 #include <aidl/android/hardware/power/BnPower.h>
@@ -47,6 +50,45 @@ using ::aidl::android::hardware::power::Boost;
 using ::ndk::ScopedAStatus;
 using ::ndk::SharedRefBase;
 
+namespace {
+
+int open_ts_input() {
+    int fd = -1;
+    DIR* dir = opendir("/dev/input");
+
+    if (dir != NULL) {
+        struct dirent* ent;
+
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_CHR) {
+                char absolute_path[PATH_MAX] = {0};
+                char name[80] = {0};
+
+                strcpy(absolute_path, "/dev/input/");
+                strcat(absolute_path, ent->d_name);
+
+                fd = open(absolute_path, O_RDWR);
+                if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
+                    if (strcmp(name, "atmel_mxt_ts") == 0 || strcmp(name, "fts_ts") == 0 ||
+                            strcmp(name, "fts") == 0 || strcmp(name, "ft5x46") == 0 ||
+                            strcmp(name, "synaptics_dsx") == 0 ||
+                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
+                        break;
+                }
+
+                close(fd);
+                fd = -1;
+            }
+        }
+
+        closedir(dir);
+    }
+
+    return fd;
+}
+
+}  // anonymous namespace
+
 namespace aidl {
 namespace android {
 namespace hardware {
@@ -60,7 +102,16 @@ void setInteractive(bool interactive) {
 ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     LOG(INFO) << "Power setMode: " << static_cast<int32_t>(type) << " to: " << enabled;
     switch(type){
-        case Mode::DOUBLE_TAP_TO_WAKE:
+        case Mode::DOUBLE_TAP_TO_WAKE: {
+            int fd = open_ts_input();
+            struct input_event ev;
+            ev.type = EV_SYN;
+            ev.code = SYN_CONFIG;
+            ev.value = enabled ? INPUT_EVENT_WAKUP_MODE_ON : INPUT_EVENT_WAKUP_MODE_OFF;
+            write(fd, &ev, sizeof(ev));
+            close(fd);
+        }
+            break;
         case Mode::LOW_POWER:
         case Mode::LAUNCH:
         case Mode::EXPENSIVE_RENDERING:
@@ -93,6 +144,7 @@ ndk::ScopedAStatus Power::isModeSupported(Mode type, bool* _aidl_return) {
     LOG(INFO) << "Power isModeSupported: " << static_cast<int32_t>(type);
 
     switch(type){
+        case Mode::DOUBLE_TAP_TO_WAKE:
         case Mode::INTERACTIVE:
         case Mode::SUSTAINED_PERFORMANCE:
         case Mode::FIXED_PERFORMANCE:
