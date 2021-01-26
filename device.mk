1f60573aa688b886b6c5c51cd45c836689da6070
palaych <palaychm@yandex.ru>
2021-01-26 16:44:40 +0100

    lmi: Enable Smart Charging
    Change-Id: I8060b41c9dc520e5b89651a4855598969f018108

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-arrow
 
 # Overlays -- Override vendor ones
 PRODUCT_PACKAGES += \
