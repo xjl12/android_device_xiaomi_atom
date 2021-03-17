01a7cf8bc307190fc32ba9f4017f9c1e5228e1c6
TH779 <tongsj2333@gmail.com>
2021-03-17 19:37:45 +0800

    lmi: init: Correct Redmi K30 Pro / Zoom Edition brand
     * Match with stock ROM.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- init/init_lmi.cpp
+++ init/init_lmi.cpp
@@ -58,7 +58,7 @@ void vendor_load_properties() {
     property_override("ro.build.description", "wayne-user 8.1.0 OPM1.171019.011 V9.5.11.0.ODCCNFA release-keys");
     property_override("ro.build.fingerprint", "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys");
     if (region.find("CN") != std::string::npos) {
-        property_override("ro.product.brand", "Xiaomi");
+        property_override("ro.product.brand", "Redmi");
         if (product.find("pro") != std::string::npos) {
             property_override("ro.product.model", "Redmi K30 Pro Zoom Edition");
             property_override("ro.product.device", "lmipro");
