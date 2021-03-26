3de186d23444f6b38b9583f212a600fbcf59e6d5
Xayah <1249475336@qq.com>
2021-03-27 01:38:45 +0800

    cezanne: Clean overlays

--- device.mk
+++ device.mk
@@ -22,7 +22,6 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-arrow
 
 # Overlays -- Override vendor ones
