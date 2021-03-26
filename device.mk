3f3a8d78703653b3fa1ed35f189a49e07e6ac86a
Xayah <1249475336@qq.com>
2021-03-26 21:41:01 +0800

    cezanne: Ship CameraGo and Gboard

--- device.mk
+++ device.mk
@@ -69,13 +69,17 @@ PRODUCT_PACKAGES += \
     BluetoothQti
 
 # Camera
-ifeq ($(ARROW_GAPPS), true)
+#ifeq ($(ARROW_GAPPS), true)
+#PRODUCT_PACKAGES += \
+#    GCamGOPrebuilt
+#else
+#PRODUCT_PACKAGES += \
+#    Snap
+#endif
+
+# Camera
 PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
-else
-PRODUCT_PACKAGES += \
-    Snap
-endif
+    CameraGo
 
 PRODUCT_COPY_FILES += \
     hardware/interfaces/camera/provider/2.4/default/android.hardware.camera.provider@2.4-service_64.rc:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/android.hardware.camera.provider@2.4-service_64.rc
@@ -91,6 +95,10 @@ PRODUCT_PACKAGES += \
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6885:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6885
+
+# Gboard
+PRODUCT_PACKAGES += \
+    Gboard
 
 # HIDL
 PRODUCT_PACKAGES += \
