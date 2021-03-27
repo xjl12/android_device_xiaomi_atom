4a3a64728742f0bfea92395cc41022c02d33313b
Xayah <1249475336@qq.com>
2021-03-28 01:54:11 +0800

    cezanne: Remove WfdCommon

--- device.mk
+++ device.mk
@@ -155,8 +155,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
