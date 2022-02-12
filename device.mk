327ea17e3a8ba5967fc8e34f864b3fcc1c604278
xjl12 <1062256455@qq.com>
2022-02-12 17:03:15 +0800

    Remove vtservice temporary for it is incompatible in A12.

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
