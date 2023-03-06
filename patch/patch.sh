32556a8fb58a01709ee690e8818d35ce13ed1baf
xjl12 <1062256455@qq.com>
2023-03-06 16:57:59 +0800

    Add patch for fingerprint

--- patch/patch.sh
+++ patch/patch.sh
@@ -2,3 +2,6 @@
 PATCH_LOC=$PWD/device/xiaomi/atom/patch
 cd device/custom/sepolicy
 git am $PATCH_LOC/0001-Fix-up-kernel-failure.patch
+cd ../../../framework/base
+git am $PATCH_LOC/0001-Fix-up-PixelExperience-permission.patch
+git am $PATCH_LOC/0002-PE-Fingerprint-fix.patch
