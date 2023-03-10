c1bcdb6a50d2eabf179d42c6876a7cf29280a993
xjl12 <1062256455@qq.com>
2023-03-10 16:48:35 +0800

    Fix vibrator HIDL problem

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,7 +117,7 @@ TARGET_USES_FOD_ZPOS := true
 
 # HIDL
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
-# DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Mediatek IMS
